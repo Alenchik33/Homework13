97c8d7d64853ef394e7b31eb580ef86471ec4f80
Alenchik <govorukhina.84@mail.ru>
2023-06-30 10:16:59 +0300

    Create project

---
+++ src/main/java/ru/netology/shop/Product.java
@@ -0,0 +1,25 @@
+package ru.netology.shop;
+
+public class Product {
+    protected int id;
+    protected String title;
+    protected int price;
+
+    public Product(int id, String title, int price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && price == product.price && title.equals(product.title);
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/test/java/ru/netology/shop/ProductTest.java
@@ -0,0 +1,35 @@
+package ru.netology.shop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+    @Test
+    public void test() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "cola", 50);
+        Product product2 = new Product(2, "fanta", 40);
+        repo.add(product1);
+        repo.add(product2);
+
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.remove(3);
+        });
+    }
+
+    @Test
+    public void test_2() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(4, "bread", 15);
+        Product product2 = new Product(5, "milk", 25);
+
+        repo.add(product1);
+        repo.add(product2);
+
+        repo.remove(4);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product2};
+        Assertions.assertArrayEquals(actual, expected);
+    }
+}
